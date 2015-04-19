5a8af3b6b62b0aa80f003ba7561d7a7405a2eaa4
lalyos <lalyos@yahoo.com>
2015-04-19 09:27:53 +0200

    cleanup

--- main.go
+++ main.go
@@ -5,35 +5,28 @@ import (
 	"math"
 )
 
-func rgb2ansi(r, g, b int) int {
-	return 16 + 36*int(r*6/256) + 6*int(g*6/256) + int(b*6/256)
-	//return 16 + 36*r*6/256 + 6*g*6/256 + b*6/256
-	//return 16 + (36*r+6*g+b)*6/256
-}
+var f float64
 
-func rgb(r, g, b int) int {
+func rgb2ansi(r, g, b int) int {
 	return 16 + 36*r + 6*g + b
 }
 
+func lolrgb(i int) (int, int, int) {
+	return int(math.Sin(f*float64(i)+0)*2.5 + 2.5),
+		int(math.Sin(f*float64(i)+2)*2.5 + 2.5),
+		int(math.Sin(f*float64(i)+4)*2.5 + 2.5)
+}
+
 func main() {
-	var r, g, b, r5, g5, b5 int
-	var f float64
+	var r, g, b int
 
 	f = 0.1
 	for i := 0; i < 50; i += 1 {
-		r = int(math.Sin(f*float64(i)+0)*127 + 128)
-		g = int(math.Sin(f*float64(i)+2)*127 + 128)
-		b = int(math.Sin(f*float64(i)+4)*127 + 128)
+		r, g, b = lolrgb(i)
 		//fmt.Printf(`<font color="%02X%02X%02X">&#9608;</font>`, r, g, b)
 
-		r5 = r * 6 / 256
-		g5 = g * 6 / 256
-		b5 = b * 6 / 256
-		_ = r5 + g5 + b5
-		//fmt.Println(r5, g5, b5, rgb(r5, g5, b5), rgb2ansi(r, g, b))
-
-		//fmt.Println(rgb(r5, g5, b5))
-		fmt.Println(rgb2ansi(r, g, b))
+		//fmt.Println(rgb2ansi(r, g, b))
+		fmt.Printf("\033[38;5;%dmo", rgb2ansi(r, g, b))
 	}
 
 }
